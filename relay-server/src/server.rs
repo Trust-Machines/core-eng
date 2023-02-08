74fe92c776e765b2be83d634aa0905e1d355c09f
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-02-08 00:08:02 -0800

    comment

--- relay-server/src/server.rs
+++ relay-server/src/server.rs
@@ -54,6 +54,7 @@ impl Server {
         ostream.flush()?;
         Ok(())
     }
+    // TODO: move this function to a `test` mod. 
     pub fn call(&mut self, msg: &[u8]) -> Result<Vec<u8>, Error> {
         let mut result = Vec::default();
         let mut stream = msg.mem_io_stream(&mut result);
