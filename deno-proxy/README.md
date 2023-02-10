b384a5f6d27ec5c12bf7c5265468405974136c04
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-02-10 13:56:54 -0800

    reading output from a process

---
+++ deno-proxy/README.md
@@ -0,0 +1,19 @@
+# Deno Proxy
+
+## Deno Installation
+
+```sh
+cargo install deno
+```
+
+## Run JS Test
+
+```sh
+deno run ./deno-proxy/test.mjs
+```
+
+## Run Rust Test
+
+```sh
+cargo run --bin deno-proxy
+```
--- deno-proxy/src/main.rs
+++ deno-proxy/src/main.rs
@@ -1,3 +1,15 @@
+use std::{process::Command, str::from_utf8};
+
+use serde_json::Value;
+
 fn main() {
-    println!("Hello, world!");
+    let v = Command::new("deno")
+        .arg("run")
+        .arg("./deno-proxy/test.mjs")
+        .output()
+        .unwrap()
+        .stdout;
+    let s = from_utf8(&v).unwrap();
+    let o = serde_json::from_str::<Value>(s).unwrap();
+    println!("{o:?}");
 }
