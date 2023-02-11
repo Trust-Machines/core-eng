79092e2adec482005f826ed89285d3aa728563f3
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-02-10 17:51:33 -0800

    it works!

--- deno-proxy/src/main.rs
+++ deno-proxy/src/main.rs
@@ -1,15 +1,83 @@
-use std::{process::Command, str::from_utf8};
+use std::{
+    io::{Error, ErrorKind, Read, Write},
+    process::{Command, Stdio, ChildStdin, ChildStdout, Child},
+    str::from_utf8,
+};
 
-use serde_json::Value;
+use serde_json::{Value, from_str};
 
-fn main() {
-    let v = Command::new("deno")
+trait ToResult {
+    type V;
+    fn to_result(self) -> Result<Self::V, Error>;
+}
+
+impl<T> ToResult for Option<T> {
+    type V = T;
+    fn to_result(self) -> Result<Self::V, Error> {
+        self.map_or(Err(Error::new(ErrorKind::InvalidData, "option")), Ok)
+    }
+}
+
+impl<T, E> ToResult for Result<T, E> {
+    type V = T;
+    fn to_result(self) -> Result<Self::V, Error> {
+        self.map_or(Err(Error::new(ErrorKind::InvalidData, "result")), Ok)
+    }
+}
+
+struct Js(Child);
+
+impl Js {
+    fn call(&mut self, v: Value) -> Result<Value, Error> {
+        let stdin = self.0.stdin.as_mut().to_result()?;
+        let r = v.to_string();
+        stdin.write(format!("{}|{}", r.len(), r).as_bytes())?;
+        stdin.flush()?;
+
+        let stdout = self.0.stdout.as_mut().to_result()?;        
+        let mut read_one = || -> Result<u8, Error> {
+            let mut a = [0];
+            stdout.read_exact(&mut a)?;
+            Ok(a[0])
+        };
+        let mut lenStr = String::default();
+        loop {
+            let c = read_one()? as char;
+            if c == '|' { break }
+            lenStr.push(c)
+        }
+        let len: usize = lenStr.parse().to_result()?;
+
+        let mut buf = Vec::default();
+        buf.resize(len, 0);
+        stdout.read_exact(&mut buf)?;
+        
+        let s = from_utf8(&buf).to_result()?;
+        let result = serde_json::from_str::<Value>(s)?;
+        Ok(result)       
+    }
+}
+
+fn f() -> Result<(), Error> {
+    let mut child = Command::new("deno")
         .arg("run")
         .arg("./deno-proxy/test.mjs")
-        .output()
-        .unwrap()
-        .stdout;
-    let s = from_utf8(&v).unwrap();
-    let o = serde_json::from_str::<Value>(s).unwrap();
-    println!("{o:?}");
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let mut js = Js(child);
+    {
+        let result = js.call(from_str("{\"a\":2}")?)?;
+        println!("{result}");
+    }
+    {
+        let result = js.call(from_str("[54]")?)?;
+        println!("{result}");
+    }
+    Ok(())
+}
+
+fn main() {
+    f().unwrap();
 }
--- deno-proxy/test.mjs
+++ deno-proxy/test.mjs
@@ -1,1 +1,44 @@
-console.log(JSON.stringify({a:5}));
+import { stdin, stdout } from 'node:process'
+
+/** @type {(v: string) => void} */
+const call = v => {
+    const s = JSON.stringify({v});
+    stdout.write(s.length + '|' + s)
+}
+
+let len = 0
+/** @type {string} */
+let buffer = ""
+
+const process = () => {
+    while (true) {
+        if (len === 0) {
+            const p = buffer.indexOf("|");
+            if (p === -1) {
+                break
+            }
+            len = parseInt(buffer.substring(0, p))
+            buffer = buffer.substring(p + 1)
+        } else {
+            if (buffer.length < len) {
+                break
+            }
+            call(buffer.substring(0, len))
+            buffer = buffer.substring(len)
+            len = 0
+        }
+    }
+}
+
+stdin.setEncoding("utf8")
+
+stdin.on('readable', () => {
+    while (true) {
+        const x = stdin.read()
+        if (x === null) { break }
+        buffer += x
+    }
+    process()
+})
+
+// while(true) {}
