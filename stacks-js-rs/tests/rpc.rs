7261cb19f1edd553e0a6d8e3a7c37d5a8d3d34e7
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-02-17 16:51:22 -0800

    burn

--- stacks-js-rs/tests/rpc.rs
+++ stacks-js-rs/tests/rpc.rs
@@ -52,14 +52,15 @@ fn test() {
     test_wrap().unwrap();
 }
 
+fn pox_address() -> PoxAddress {
+    PoxAddress::Standard(StacksAddress::new(0, Hash160::from_data(&[0; 20])), None)
+}
+
 #[test]
-fn in_test() {
+fn mirror_peg_in_op_test() {
     let p = PegInOp {
         recipient: PrincipalData::Standard(StandardPrincipalData(0, [0; 20])),
-        peg_wallet_address: PoxAddress::Standard(
-            StacksAddress::new(0, Hash160::from_data(&[0; 20])),
-            None,
-        ),
+        peg_wallet_address: pox_address(),
         amount: 0,
         memo: Vec::default(),
         txid: Txid([0; 32]),
@@ -74,8 +75,25 @@ fn in_test() {
     assert_eq!(to_string(&result).unwrap(), expected);
 }
 
-fn pox_address() -> PoxAddress {
-    PoxAddress::Standard(StacksAddress::new(0, Hash160::from_data(&[0; 20])), None)
+#[test]
+fn mirror_peg_out_request_op_test() {
+    let p = PegOutRequestOp {
+        amount: 0,
+        recipient: pox_address(),
+        signature: MessageSignature([0; 65]),
+        peg_wallet_address: pox_address(),
+        fulfillment_fee: 0,
+        memo: Vec::default(),
+        txid: Txid([0; 32]),
+        vtxindex: 0,
+        block_height: 0,
+        burn_header_hash: BurnchainHeaderHash([0; 32]),
+    };
+    let x = In::Burn(&p);
+    let mut js = Js::new("./mirror.ts").unwrap();
+    let result = js.call::<_, Value>(&x).unwrap();
+    let expected = r#"[{"Burn":{"amount":0,"block_height":0,"burn_header_hash":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"fulfillment_fee":0,"memo":[],"peg_wallet_address":{"Standard":[{"bytes":"944f997c5553a6f3e1028e707c71b5fa0dd3afa7","version":0},null]},"recipient":{"Standard":[{"bytes":"944f997c5553a6f3e1028e707c71b5fa0dd3afa7","version":0},null]},"signature":"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000","txid":"0000000000000000000000000000000000000000000000000000000000000000","vtxindex":0}}]"#;
+    assert_eq!(to_string(&result).unwrap(), expected);
 }
 
 #[test]
