7a2d030fd4d848f41945628e300ef9ce45d44fd7
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-02-17 16:13:55 -0800

    Mint

--- stacks-js-rs/stacks.ts
+++ stacks-js-rs/stacks.ts
@@ -1,11 +1,48 @@
 import { listenStdio, JsonMap, Json } from './rpc.ts'
 
 // Example:
-// {"Mint":{"amount":0,"block_height":0,"burn_header_hash":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"memo":[],"peg_wallet_address":{"Standard":[{"bytes":"944f997c5553a6f3e1028e707c71b5fa0dd3afa7","version":0},null]},"recipient":{"Standard":[0,[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]},"txid":"0000000000000000000000000000000000000000000000000000000000000000","vtxindex":0}}
+//  {
+//      "Mint":{
+//          "amount":0,
+//          "block_height":0,
+//          "burn_header_hash":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+//          "memo":[],
+//          "peg_wallet_address":{
+//              "Standard":[
+//                  {"bytes":"944f997c5553a6f3e1028e707c71b5fa0dd3afa7","version":0},
+//                  null
+//              ]
+//          },
+//          "recipient":{
+//              "Standard": [0,[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+//          },
+//          "txid":"0000000000000000000000000000000000000000000000000000000000000000",
+//          "vtxindex":0
+//      }
+//  }
 
 type Command = { readonly Mint: Mint } | { readonly Burn: Burn }
 
-type Mint = Json
+type Mint = {
+    readonly amount: number,
+    readonly block_height: number,
+    readonly burn_header_hash: readonly number[],
+    readonly memo: readonly number[],
+    readonly peg_wallet_address: {
+        readonly Standard: readonly[
+            {
+                readonly bytes: string,
+                readonly version: string,
+            },
+            null
+        ],
+    },
+    readonly recipient: {
+        readonly Snadard: readonly[number, readonly number[]]
+    },
+    readonly txid: string,
+    readonly vtxindex: number,
+}
 
 type Burn = Json
 
