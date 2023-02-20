00883ec2b54013fbc4a552f7a822d14cb983e46d
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-02-20 08:54:35 -0800

    add IO errors

--- stacks-js-rs/src/stacks_wallet_js.rs
+++ stacks-js-rs/src/stacks_wallet_js.rs
@@ -1,9 +1,9 @@
-use std::io::Error;
+use std::io;
 
 use serde::Serialize;
 use stacks_coordinator::{
     peg_wallet::StacksWallet,
-    stacks_node::{PegInOp, PegOutRequestOp, StacksTransaction},
+    stacks_node::{PegInOp, PegOutRequestOp},
 };
 
 use crate::Js;
@@ -11,19 +11,19 @@ use crate::Js;
 pub struct StacksWalletJs(Js);
 
 impl StacksWallet for StacksWalletJs {
-    fn mint(&mut self, op: &stacks_coordinator::stacks_node::PegInOp) -> String {
-        self.0.call(&In::Mint(op)).unwrap()
+    fn mint(&mut self, op: &stacks_coordinator::stacks_node::PegInOp) -> io::Result<String> {
+        self.0.call(&In::Mint(op))
     }
 
-    fn burn(&mut self, op: &stacks_coordinator::stacks_node::PegOutRequestOp) -> String {
-        self.0.call(&In::Burn(op)).unwrap()
+    fn burn(&mut self, op: &stacks_coordinator::stacks_node::PegOutRequestOp) -> io::Result<String> {
+        self.0.call(&In::Burn(op))
     }
 
     fn set_wallet_address(
         &mut self,
         address: stacks_coordinator::peg_wallet::PegWalletAddress,
-    ) -> String {
-        self.0.call(&In::SetWalletAddress(&address)).unwrap()
+    ) -> io::Result<String> {
+        self.0.call(&In::SetWalletAddress(&address))
     }
 }
 
