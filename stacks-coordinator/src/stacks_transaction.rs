b10275533b9ebd8b18d9ffa2ee7b39037bbb8ffb
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-03-02 07:12:49 -0800

    minor comment

--- stacks-coordinator/src/stacks_transaction.rs
+++ stacks-coordinator/src/stacks_transaction.rs
@@ -5,7 +5,8 @@ use crate::make_contract_call::{
     TransactionVersion,
 };
 
-// TODO: Find appropriate type that's compatible with stacks.js JSON
+/// Current type is compatible with stacks.js JSON
+/// TODO: Find appropriate type
 #[allow(non_snake_case)]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct StacksTransaction {
